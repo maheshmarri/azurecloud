3f7454290225ea7a2e61b8b33d4cf8c6a4514e2d
Mahesh Marri <mahesh.marri@gmail.com>
2018-11-14 14:19:02 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-   echo "Hello !! World";
-    phpInfo();
+echo "<html>\n";
+echo "<head>\n";
+echo "<title>My Second PHP Example</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "<p>Some Content</p>\n";
+echo "</body>\n";
+echo "</html>\n";
 ?>
-
